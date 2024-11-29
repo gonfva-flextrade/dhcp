37b0b108522050710bd18c06ed737203c9a0fb23
gonfva-flextrade <164539895+gonfva-flextrade@users.noreply.github.com>
2024-11-29 17:10:29 +0000

    Update conn_windows.go

--- dhcpv4/server4/conn_windows.go
+++ dhcpv4/server4/conn_windows.go
@@ -1,11 +1,34 @@
+// +build windows
+
 package server4
 
 import (
-	"errors"
+	"fmt"
 	"net"
 )
 
-// NewIPv4UDPConn fails on Windows. Use WithConn() to pass the connection.
+// NewIPv4UDPConn returns a UDP connection bound to both the interface and port
+// given based on a IPv4 UDP socket. The UDP connection allows broadcasting.
 func NewIPv4UDPConn(iface string, addr *net.UDPAddr) (*net.UDPConn, error) {
-	return nil, errors.New("not implemented on Windows")
+	// For Windows, we use the ListenPacket method to create a UDP connection.
+	// This method automatically handles socket creation and configuration.
+	laddr := net.UDPAddr{
+		IP:   net.IPv4zero, // Use IPv4zero for listening on all interfaces.
+		Port: addr.Port,
+	}
+	conn, err := net.ListenPacket("udp4", laddr.String())
+	if err != nil {
+		return nil, fmt.Errorf("cannot create UDP listener: %v", err)
+	}
+	udpConn, ok := conn.(*net.UDPConn)
+	if !ok {
+		return nil, fmt.Errorf("incorrect connection type: %T, expected *net.UDPConn", conn)
+	}
+	// On Windows, binding to a specific interface is not as straightforward
+	// as on Unix-based systems. It might require additional steps or be
+	// managed differently depending on the use case and network configuration.
+	// Setting socket options like SO_BROADCAST is not directly exposed in the
+	// net package, but the ListenPacket should create a socket that is
+	// capable of broadcasting by default.
+	return udpConn, nil
 }
